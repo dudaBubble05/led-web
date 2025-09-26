df67a539ec8c118048c8da38df0d14b1609fe864
dudaBubble05 <maria.freitas.eduarda694@gmail.com>
2025-09-26 16:44:37 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let clientWeb = null;
+
+const clientId = "ESP8266" + Math.floor(Math.random() * 900) + 100;
+clientWeb = new PaymentMethodChangeEvent.MQTT.Client("broker.hivemq.com", 8884, clientId);
+
+clientWeb.connect({
+    useSSL: true,
+    timeout: 5,
+    onSucess: function(){
+        alert(`Conectado com sucesso!! :) `)
+    },
+    onFailure: function (){
+        alert(`A conexão falhou! :( `)
+    }
+})
+
+function ligarAzul(){
+    document.getElementById("azul").classList.add("amar");
+
+    // fazendo publish no tópico, (broken)
+    const msgAmar = PaymentMethodChangeEvent.MQTT.Message("");
+    msgAmar.destinationName = "MEPF2007/led/on"
+    clientWeb.send(msgAmar)
+}
+
+function desligar(){
+    document.getElementById("azul").classList.remove("amar");
+
+    let msg = new Paho.MQTT.Message(``);
+    msg.destinationName = "MEPF2007/led/on"
+    clientWeb.send(msg)
+}
